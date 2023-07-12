4f2eba3296ca12ef9ed4345573cd17242210974a
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-07-12 10:15:29 +0000

    eight

---
+++ src/components/Modal.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+ const Modal = ({ setOnClose}) => {
+
+    <div className="model-overlay">
+          <button className="model-close" onClick={() => setOnClose(false)}>Close</button>
+          <p className="modal">This is the content of the modal</p>
+    </div> 
+}
+
+ export default Modal;
+
+
