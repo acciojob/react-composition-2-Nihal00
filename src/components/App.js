b396ecc0351151f55d9f3758f447b7fb09247564
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-07-12 09:56:03 +0000

    seven

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,14 @@ const App = () => {
 
   return (
       <div>
-        <button onClick={() => setOnClose(true)}>Show Modal</button>
-
-        <div className="model-overlay">
+        <div>
+          <button onClick={() => setOnClose(true)}>Show Modal</button>
+        </div>
+        
+        {onClose ? <div className="model-overlay">
           <button className="model-close" onClick={() => setOnClose(false)}>Close</button>
           <p className="modal">This is the content of the modal</p>
-        </div>
+        </div> : ""}
 
       </div>
     
